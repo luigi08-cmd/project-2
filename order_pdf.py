fc436ff90c0891b746ddf5ee50af6c0c7c6474ca
luigi08-cmd <99079945@mydavinci.nl>
2025-01-30 11:11:06 +0100

    json

--- order_pdf.py
+++ order_pdf.py
@@ -6,28 +6,56 @@ import shutil
 import json
 
 # makes the main folder
-folder_location = os.path.dirname(__file__) + r"\PDF_INVOICE"
-if os.path.exists(folder_location):
-    shutil.rmtree(folder_location)
-os.makedirs(folder_location)
+pdf_folder_location = os.path.dirname(__file__) + r"\PDF_INVOICE"
+if os.path.exists(pdf_folder_location):
+    shutil.rmtree(pdf_folder_location)
+os.makedirs(pdf_folder_location)
 
-# creates pdf
+json_folder_location = os.path.dirname(__file__) + r"\JSON_INVOICE"
+if os.path.exists(json_folder_location):
+    shutil.rmtree(json_folder_location)
+os.makedirs(json_folder_location)
+
 def create_pdf(json_file):
 
+    # create json
     with open('2000-096.json') as file:
         data =json.load(file)
 
     order = data["order"]
     klant = order["klant"]
 
-    totale_prijs_incl_btw = 0
+    totale_prijs_excl_btw = 0
+
+    # create file location with correct json name
+    pdf_name = json_file + ".json"
+    file_location = os.path.join(json_folder_location , pdf_name)
+    
+    # klant gegevens
+    bedrijfsnaam = klant["naam"]
+    address_klant = klant["adres"]
+    poscode_klant = klant["postcode"]
+    vestegings_plaats_klant = klant["stad"]
+    factuur_nummer = order["ordernummer"]
+    order_datum = order["orderdatum"]
+    betaal_termijn = order["betaaltermijn"]
+    
+    for index in range(0, len(order["producten"])):
+        product["aantal"]
+        product["productnaam"]
+        btw = 1 + product["btw_percentage"] / 100
+        product_prijs = product["prijs_per_stuk_excl_btw"] * product["aantal"]
+        product["prijs_per_stuk_excl_btw"]
+
+
+    # create pdf
 
     # default sizes
     width, height = A4
 
     # create file location with correct pdf name
     pdf_name = json_file + ".pdf"
-    file_location = os.path.join(folder_location , pdf_name) 
+    file_location = os.path.join(pdf_folder_location , pdf_name) 
     canvas = Canvas(file_location)
     logo_location = os.path.join(os.path.dirname(__file__), "logo.png")
 
@@ -50,47 +78,42 @@ def create_pdf(json_file):
     canvas.line(0.5 * inch, height - 2.7 * inch, 7.5 * inch, height - 2.7 * inch)
     
     
-    # klant gegevens
-    bedrijfsnaam = klant["naam"]
-    address_klant = klant["adres"]
-    poscode_klant = klant["postcode"]
-    vestegings_plaats_klant = klant["stad"]
-    kvk_nummer = klant["KVK-nummer"]
-    factuur_nummer = order["ordernummer"]
-    order_datum = order["orderdatum"]
-    betaal_termijn = order["betaaltermijn"]
+    
 
     canvas.drawString(1 * inch, height - 3 * inch, bedrijfsnaam)
     canvas.drawString(1 * inch, height - 3.2 * inch, address_klant)
     canvas.drawString(1 * inch, height - 3.4 * inch, poscode_klant)
     canvas.drawString(1 * inch, height - 3.6 * inch, vestegings_plaats_klant)
-    canvas.drawString(1 * inch, height - 4 * inch, kvk_nummer)
-    canvas.drawString(1 * inch, height - 4.4 * inch, factuur_nummer)
-    canvas.drawString(1 * inch, height - 4.6 * inch, order_datum)
-    canvas.drawString(1 * inch, height - 4.8 * inch, betaal_termijn)
+    canvas.drawString(1 * inch, height - 4.0 * inch, factuur_nummer)
+    canvas.drawString(1 * inch, height - 4.2 * inch, order_datum)
+    canvas.drawString(1 * inch, height - 4.4 * inch, betaal_termijn)
 
     # algemene product gegevens
-    canvas.line(0.5 * inch, height - 5 * inch, 7.5 * inch, height - 5 * inch)
-    canvas.drawString(1 * inch, height - 5.2 * inch, "aantal")
-    canvas.drawString(2 * inch, height - 5.2 * inch, "omschrijving")
-    canvas.drawString(5 * inch, height - 5.2 * inch, "prijs")
-    canvas.drawString(5.6 * inch, height - 5.2 * inch, "totaal excl. btw")
-    canvas.line(0.5 * inch, height - 5.3 * inch, 7.5 * inch, height - 5.3 * inch)
+    canvas.line(0.5 * inch, height - 4.5 * inch, 7.5 * inch, height - 4.5 * inch)
+    canvas.drawString(1 * inch, height - 4.7 * inch, "aantal")
+    canvas.drawString(2 * inch, height - 4.7 * inch, "omschrijving")
+    canvas.drawString(5 * inch, height - 4.7 * inch, "prijs")
+    canvas.drawString(5.6 * inch, height - 4.7 * inch, "totaal excl. btw")
+    canvas.line(0.5 * inch, height - 4.8 * inch, 7.5 * inch, height - 4.8 * inch)
 
     # product gegevens 
-    for product in order["producten"]:
-        canvas.drawString(1 * inch, height - 5.6 * inch, str(product["aantal"]))
-        canvas.drawString(2 * inch, height - 5.6 * inch, product["productnaam"])
-        btw = 1 + product["btw_percentage"] / 100
-        product_prijs = product["prijs_per_stuk_excl_btw"] * product["aantal"] * btw
-        canvas.drawString(4.7 * inch, height - 5.6 * inch, f"€ {str(product["prijs_per_stuk_excl_btw"])}")
-        canvas.drawString(5.6 * inch, height - 5.6 * inch, f"€ str(product_prijs)")
-        totale_prijs_incl_btw += product_prijs
+    for index in range(0, len(order["producten"])):
+        product = order["producten"][index]
+        canvas.drawString(1 * inch, height - (5 + 0.2 * index) * inch, str())
+        canvas.drawString(2 * inch, height - (5 + 0.2 * index) * inch, )
+        
+        canvas.drawString(4.7 * inch, height - (5 + 0.2 * index) * inch, f"€ {str()}")
+        canvas.drawString(5.6 * inch, height - (5 + 0.2 * index) * inch, f"€ {str(product_prijs)}")
+        totale_prijs_excl_btw += product_prijs
+        stop = index + 1
+        if stop == len(order["producten"]):
+            # totaal
+            canvas.line(0.5 * inch, height - (5.1 + 0.2 * index) * inch, 7.5 * inch, height - (5.1 + 0.2 * index) * inch)    
+            canvas.drawString(5 * inch, height - (5.3 + 0.2 * index)* inch, "totaal")
+            canvas.drawString(5.6 * inch, height - (5.3 + 0.2 * index) * inch, f"€ {str(totale_prijs_excl_btw)}")
 
-    # totaal
-    canvas.line(0.5 * inch, height - 5.7 * inch, 7.5 * inch, height - 5.7 * inch)
-    canvas.drawString(5 * inch, height - 5.9 * inch, "totaal")
-    canvas.drawString(5.6 * inch, height - 5.9 * inch, f"€ {str(totale_prijs_incl_btw)}")
+
+
 
     canvas.save()
 create_pdf("test")
